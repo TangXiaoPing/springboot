579e8cfccd728b33556e5bf3b3fa214fcad651ef
tangxiaoping <15073504659@163.com>
2019-05-07 23:40:45 +0800

    修改首页

--- src/main/resources/static/js/visitorList.js
+++ src/main/resources/static/js/visitorList.js
@@ -1,60 +1,60 @@
 var pageCurr;
 $(function () {
     layui.use('table', function () {
-        var table = layui.table
-            , form = layui.form;
+            var table = layui.table
+                , form = layui.form;
 
-        tableIns = table.render({
-            id: "visitorTableId"
-            , elem: '#visitorList'
-            , url: '/visitor/getVisitorList'
-            , method: 'get' //默认：get请求
-            , cellMinWidth: 80
-            , page: true
-            , request: {
-                pageName: 'page' //页码的参数名称，默认：page
-                , limitName: 'limit' //每页数据量的参数名，默认：limit
-            }, response: {
-                statusName: 'code' //数据状态的字段名称，默认：code
-                , statusCode: 200 //成功的状态码，默认：0
-                , countName: 'totals' //数据总数的字段名称，默认：count
-                , dataName: 'list' //数据列表的字段名称，默认：data
-            }
-            , cols: [[ //表头
-                {type: 'checkbox'}
-                , {field: 'visitorName', title: '访问者姓名', sort: true}
-                , {field: 'visitorPhone', title: '访问者电话'}
-                , {field: 'dormRoomId', title: '访问宿舍id'}
-                , {field: 'studentId', title: '访问学生id'}
-                , {
-                    field: 'visitorTime',
-                    title: '访问时间',
-                    templet: '<div>{{ Format(d.visitorTime,"yyyy-MM-dd hh:mm:ss")}}</div>}'
+            tableIns = table.render({
+                id: "visitorTableId"
+                , elem: '#visitorList'
+                , url: '/visitor/getVisitorList'
+                , method: 'get' //默认：get请求
+                , cellMinWidth: 80
+                , page: true
+                , request: {
+                    pageName: 'page' //页码的参数名称，默认：page
+                    , limitName: 'limit' //每页数据量的参数名，默认：limit
+                }, response: {
+                    statusName: 'code' //数据状态的字段名称，默认：code
+                    , statusCode: 200 //成功的状态码，默认：0
+                    , countName: 'totals' //数据总数的字段名称，默认：count
+                    , dataName: 'list' //数据列表的字段名称，默认：data
                 }
-                , {
-                    field: 'goAwayTime',
-                    title: '离开时间',
-                    templet: '<div>{{ Format(d.goAwayTime,"yyyy-MM-dd hh:mm:ss")}}</div>}'
-                }
-                , {fixed: 'right', align: 'center', toolbar: '#optBar'}
-            ]]
-            , done: function (res, curr, count) {
-                //如果是异步请求数据方式，res即为你接口返回的信息。
-                //如果是直接赋值的方式，res即为：{data: [], count: 99} data为当前页数据、count为数据总长度
-                // console.log(res);
-                //得到当前页码
-                // console.log(curr);
-                //得到数据总量
-                // console.log(count);
-                $("[data-field='status']").children().each(function () {
-                    if ($(this).text() == '0') {
-                        $(this).text("已处理")
-                    } else {
-                        $(this).text("未处理")
+                , cols: [[ //表头
+                    {type: 'checkbox'}
+                    , {field: 'visitorName', title: '访问者姓名', sort: true}
+                    , {field: 'visitorPhone', title: '访问者电话'}
+                    , {field: 'dormRoomId', title: '访问宿舍id'}
+                    , {field: 'studentId', title: '访问学生id'}
+                    , {
+                        field: 'visitorTime',
+                        title: '访问时间',
+                        templet: '<div>{{ Format(d.visitorTime,"yyyy-MM-dd hh:mm:ss")}}</div>}'
                     }
-                });
-            }
-        });
+                    , {
+                        field: 'goAwayTime',
+                        title: '离开时间',
+                        templet: '<div>{{ Format(d.goAwayTime,"yyyy-MM-dd hh:mm:ss")}}</div>}'
+                    }
+                    , {fixed: 'right', align: 'center', toolbar: '#optBar'}
+                ]]
+                , done: function (res, curr, count) {
+                    //如果是异步请求数据方式，res即为你接口返回的信息。
+                    //如果是直接赋值的方式，res即为：{data: [], count: 99} data为当前页数据、count为数据总长度
+                    // console.log(res);
+                    //得到当前页码
+                    // console.log(curr);
+                    //得到数据总量
+                    // console.log(count);
+                    $("[data-field='status']").children().each(function () {
+                        if ($(this).text() == '0') {
+                            $(this).text("已处理");
+                        } else {
+                            $(this).text("未处理");
+                        }
+                    });
+                }
+            });
 
         //监听工具条
         table.on('tool(visitorTable)', function (obj) {
