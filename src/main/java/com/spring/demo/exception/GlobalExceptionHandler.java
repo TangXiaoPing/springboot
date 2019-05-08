d5ac9ae30ef18325123c0651451f99b76fc53e6a
tangxiaoping <15073504659@163.com>
2019-05-08 13:42:12 +0800

    优化全局异常处理

--- src/main/java/com/spring/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/spring/demo/exception/GlobalExceptionHandler.java
@@ -3,8 +3,10 @@ package com.spring.demo.exception;
 import com.spring.demo.constant.UcenterResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -36,6 +38,34 @@ public class GlobalExceptionHandler {
             builder.append(error.getDefaultMessage() + "\n");
         }
         return new UcenterResult(builder.toString());
+    }
+
+    @ExceptionHandler(BindException.class)
+    public UcenterResult bindExceptionHandler(BindException ex) {
+        String message = getMessage(ex.getAllErrors());
+        if (message == null) {
+            message = ex.getMessage();
+        }
+        return new UcenterResult(message);
+    }
+
+    private String getMessage(List<ObjectError> errors) {
+        if (errors == null || errors.isEmpty()) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        for (ObjectError error : errors) {
+            if (sb.length() > 0) {
+                sb.append("; ");
+            }
+            if (error instanceof FieldError) {
+                FieldError fe = (FieldError) error;
+                sb.append(fe.getField()).append(fe.getDefaultMessage());
+            } else {
+                sb.append(error.getDefaultMessage());
+            }
+        }
+        return sb.toString();
     }
 
     /**
